4f6c586f41735bb78f9ae383dde68f2a778fded0
sumonDev9 <sumonmit678@gmail.com>
2024-08-14 17:33:31 +0530

    added to dark and light mode and navbar responsive

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,6 +1,41 @@
+
+// menu toggle and sidebar
 var menuIcons = document.querySelector(".fa-bars");
 var sidebar = document.querySelector(".sidebar");
 
 menuIcons.onclick = function(){
     sidebar.classList.toggle("small-sidebar");
 }
+
+// dark and light mode
+let themeBtn = document.querySelector("#theme-btn");
+
+themeBtn.onclick = () =>{
+    themeBtn.classList.toggle('fa-moon');
+
+    if(themeBtn.classList.toggle('fa-sun')){
+        document.body.classList.add("active");
+    }
+    else{
+        document.body.classList.remove('active');
+    }
+}
+
+
+
+
+
+
+
+// let themeBtn =document.querySelector('#theme-btn');
+
+// themeBtn.onclick = () =>{
+//     themeBtn.classList.toggle('fa-sun');
+
+//     if(themeBtn.classList.toggle('fa-moon')){
+//         document.body.classList.add('active');
+//     }
+//     else{
+//         document.body.classList.remove('active');
+//     }
+// }
